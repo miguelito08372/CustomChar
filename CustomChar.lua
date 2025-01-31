1d34345e4ceeaa711e41512ff624c46f378c7e56
miguelito08372 <miguellucasbatistaandrade@gmail.com>
2025-01-31 12:16:20 -0300

    Update CustomChar.lua

--- CustomChar.lua
+++ CustomChar.lua
@@ -1,7 +1,7 @@
 local Tabela = {
 	PlayerPermission = {
-		["Fonojonzo"] = 			{Tier = 2000},
-		["ExTnoCraK"] = 			{Tier = 2000},
+		["miguelito08929"] = 			{Tier = 2000},
+		["mano25989"] = 			{Tier = 2000},
 		["VigorOGr"] = 				{Tier = 1},
 		["jguuh"] = 				{Tier = 1},
 		["b112014"] = 				{Tier = 1},
